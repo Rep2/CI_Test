024ddf1efffd367f5338b1b783f9ff8a22d3eb2e
Ivan Rep <ivan.rep@undabot.com>
2016-11-04 14:24:27 +0100

    InitialCommit

---
+++ CITest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CITest
+//
+//  Created by Undabot Rep on 04/11/2016.
+//  Copyright © 2016 undabot. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
