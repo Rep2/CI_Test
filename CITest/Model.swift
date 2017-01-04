f36c91e9ec510b1fc67346b6691f05769ce95025
Ivan Rep <ivan.rep@undabot.com>
2017-01-04 16:09:20 +0100

    Removed TweetFeedModels

--- CITest/Model.swift
+++ CITest/Model.swift
@@ -1,16 +1,16 @@
-import TweetFeedModels
+//import TweetFeedModels
 import Unbox
 import Alamofire
 
 struct Model {
     let name: String
 
-    let size: Size
+   // let size: Size
 }
 
 extension Model: Unboxable {
     init(unboxer: Unboxer) throws {
-        self.size = try unboxer.unbox(key: "size")
+     //   self.size = try unboxer.unbox(key: "size")
         self.name = try unboxer.unbox(key: "name")
     }
 }
