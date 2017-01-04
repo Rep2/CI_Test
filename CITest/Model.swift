2b9991155332f569870678d4bc33f2918d7669fa
Ivan Rep <ivan.rep@undabot.com>
2017-01-04 16:27:45 +0100

    Removed tweet feed modules

--- CITest/Model.swift
+++ CITest/Model.swift
@@ -1,16 +1,12 @@
-//import TweetFeedModels
 import Unbox
 import Alamofire
 
 struct Model {
     let name: String
-
-   // let size: Size
 }
 
 extension Model: Unboxable {
     init(unboxer: Unboxer) throws {
-     //   self.size = try unboxer.unbox(key: "size")
         self.name = try unboxer.unbox(key: "name")
     }
 }
--- CITestTests/CITestUnitTest.swift
+++ CITestTests/CITestUnitTest.swift
@@ -2,30 +2,26 @@ import Foundation
 import XCTest
 import Unbox
 @testable import CITest
-import TweetFeedModels
 
 class CITestUnitTest: XCTestCase {
 
     func test_toJSON() {
-        let model = Model(name: "test", size: Size(width: 10, height: 10))
+        let model = Model(name: "test")
 
         let json = model.toJSONDict()
 
         XCTAssert(json["name"] as? String == "test")
-        XCTAssert(json["size"] as? Size == Size(width: 10, height: 10))
     }
 
     func test_fromJSON() {
         let json: [String : Any] = [
-            "name" : "test" as Any,
-            "size" : ["width" : 10, "height" : 10] as Any
+            "name" : "test" as Any
         ]
 
         do {
             let model: Model = try unbox(data: JSONSerialization.data(withJSONObject: json, options: .prettyPrinted))
 
             XCTAssert(model.name == "test")
-            XCTAssert(model.size == Size(width: 10, height: 10))
         } catch {
             XCTAssertFalse(true)
         }
